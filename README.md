517a97ed670c54b2b65b82d2331a4180cfbce00c
Kevin Edmond <Kedmond24@live.com>
2019-07-22 21:48:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Web App Project
+----------------
+The Sports Roster App
+
+
+Purpose
+----------------
+The Sports Roster App is a web application that uses Python, Flask, HTML, and CSS to list the current roster for every NBA Team.
+
+
+## Requirements
+----------------
+The following software is needed to run this project:
+
+- [Python 3](https://www.python.org/downloads/release/python-370/)
+- [Flask](http://flask.pocoo.org/)
+- [HTML5](https://html5.org/)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS/)
+
+
+## Running The Sports Roster App
+--------------------------------------------
+### Project Files
+   - `app.py` Python web application server file
+   - `templates folder` Folder housing HTML templates
+   - `static folder` Folder housing CSS
+   - `README.md` README documentation
+
+### Open a browser window
---
+++ app.py
@@ -0,0 +1,53 @@
+# Import Required Libraries
+from flask import (Flask, render_template, request, redirect, url_for, flash, jsonify)
+from flask import session as loginSession, make_response
+import json
+import requests
+
+
+app = Flask(__name__)
+Application_Name = "Sports Roster App"
+
+
+# Establish Connection to API Source
+t = requests.get('https://www.balldontlie.io/api/v1/teams')
+teamData = t.json()
+
+p = requests.get('https://www.balldontlie.io/api/v1/players')
+playerData = p.json()
+
+
+# Assist Functions
+def teamList():
+    list_team = []
+    for i in teamData['data']:
+        list_team.append(i['full_name'])
+    return list_team
+
+def playerList(teamName):
+    onRoster = []
+    for p in playerData['data']:
+        if p['team']['full_name'] == teamName:
+            onRoster.append(p)
+
+
+# App Routes
+# Method for Displaying Home and Team Nav Menu
+@app.route('/')
+def teamMenu():
+    return render_template('index.html', teams=teamList())
+
+# Method for Displaying Selected Team's Roster
+@app.route('/show/<teamName>')
+def playerList(teamName):
+    onRoster = []
+    for p in playerData['data']:
+        if p['team']['full_name'] == teamName:
+            onRoster.append(p)
+    return render_template('show.html', teams=teamList(), players=onRoster, currentTeam=teamName)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
