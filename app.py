3ab32d0fc0c58935a65480dfe216c5d4a186029b
Kevin Edmond <Kedmond24@live.com>
2019-07-27 16:36:40 -0400

    ADD functionality for multipage player data api call

--- app.py
+++ app.py
@@ -9,24 +9,42 @@ app = Flask(__name__)
 Application_Name = "Sports Roster App"
 
 
-# Establish Connection to API Source
+###### Establish Connection to API Source
+# Get Team API data
 t = requests.get('https://www.balldontlie.io/api/v1/teams')
 teamData = t.json()
 
-p = requests.get('https://www.balldontlie.io/api/v1/players')
-playerData = p.json()
+# Get Player API data
+def getPlayerData():
+    playerResults = []
+    try:
+        for pg_num in range(1, 33):
+            url = "https://www.balldontlie.io/api/v1/players?per_page=100&page=" + str(pg_num)
+            r = requests.get(url)
+            r_json = r.json()
+            playerResults = playerResults + r_json['data']
+        return playerResults
+    except Exception as e:
+        print("This is a Player Data error!")
 
 
-# Assist Functions
+###### Assist Functions
+# Placeholder for returned player API data
+playerData = getPlayerData()
+
+# Filter team API data for navbar
 def teamList():
-    list_team = []
-    for i in teamData['data']:
-        list_team.append(i['full_name'])
-    return list_team
+    try:
+        list_team = []
+        for i in teamData['data']:
+            list_team.append(i['full_name'])
+        return list_team
+    except Exception as e:
+        print("This is a Team Data error!")
 
 
-# App Routes
-# Method for Displaying Home and Team Nav Menu
+###### App Routes
+# Method for Displaying Homepage and Team Nav Menu
 @app.route('/')
 def teamMenu():
     return render_template('index.html', teams=teamList())
@@ -34,10 +52,14 @@ def teamMenu():
 # Method for Displaying Selected Team's Roster
 @app.route('/show/<teamName>')
 def playerList(teamName):
+    print(teamName)
+    # playerData = playerTest()
     onRoster = []
-    for p in playerData['data']:
-        if p['team']['full_name'] == teamName:
-            onRoster.append(p)
+    for p in range(len(playerData)):
+        if playerData[p]['team']['full_name'] == teamName:
+            onRoster.append(playerData[p])
+            if len(onRoster) == 18:
+                break
     return render_template('show.html', teams=teamList(), players=onRoster, currentTeam=teamName)
 
 
