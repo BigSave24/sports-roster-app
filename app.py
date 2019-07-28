e7ee4da0677e0d9fd1a75ac564d862cab3182166
Kevin Edmond <Kedmond24@live.com>
2019-07-28 14:01:49 -0400

    FIX playerList function added explicit call to playerData

--- app.py
+++ app.py
@@ -52,10 +52,11 @@ def teamMenu():
 # Method for Displaying Selected Team's Roster
 @app.route('/show/<teamName>')
 def playerList(teamName):
+    getRoster = playerData
     onRoster = []
-    for p in range(len(playerData)):
-        if playerData[p]['team']['full_name'] == teamName:
-            onRoster.append(playerData[p])
+    for p in range(len(getRoster)):
+        if getRoster[p]['team']['full_name'] == teamName:
+            onRoster.append(getRoster[p])
             if len(onRoster) == 18:
                 break
     return render_template('show.html', teams=teamList(), players=onRoster, currentTeam=teamName)
